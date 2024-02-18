82dd2ea61890e95581eb0a24d342b67843c28693
Sycinz <gronbosof@gmail.com>
2024-02-18 13:06:09 +0100

    Fixed and more flexible code

--- src/main.rs
+++ src/main.rs
@@ -2,17 +2,22 @@ use std::io;
 use bcrypt::hash;
 
 fn main() {
-    println!("Please enter your password, that you wanna encrypt");
-    println!("...");
-
     // Getting user input from get_user_input function
-    let passwd: String = get_user_input();
+    let passwd = get_user_input("password");
 
-    println!("Please enter your wanted hash type: ");
-    println!("...");
+    if passwd.is_empty() {
+        eprintln!("Error: Password cannot be empty.");
+        main();
+    }
 
     // Getting user input as int (to declare hash type)
-    let user_cost: u32 = get_user_cost();
+    let user_cost: u32 = match get_user_input("hash cost").trim().parse() {
+        Ok(cost) => cost,
+        Err(err) => {
+            eprintln!("Error parsing hash cost: {}", err);
+            return;
+        }
+    };
 
     // Using match to hash the password with user_cost hashing (which is 12 by default)
     match hash(passwd, user_cost) {
@@ -24,22 +29,14 @@ fn main() {
     };
 }
 
-fn get_user_input() -> String {
+fn get_user_input(prompt: &str) -> String {
     let mut user_input = String::new();
 
-    io::stdin()
-        .read_line(&mut user_input)
-        .unwrap();
-
-    user_input
-} 
-
-fn get_user_cost() -> u32 {
-    let mut user_input = String::new();
+    println!("Please enter your {}: ", prompt);
 
     io::stdin()
-        .read_line(&mut user_input)
-        .unwrap();
+    .read_line(&mut user_input)
+    .expect("Failed to readline");
 
-    user_input.trim().parse().unwrap()
+    user_input.trim().to_string()
 }
